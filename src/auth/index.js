a06531b9a14c2d20790a8b9bc543924a97abed21
Andre Pato <anpato1994@gmail.com>
2019-11-27 11:06:21 -0500

    changed auth file to Authservice class

--- src/auth/index.js
+++ src/auth/index.js
@@ -6,48 +6,50 @@ const jwt = jsonwebtoken
 const TOKEN_KEY = process.env.TOKEN_KEY
 const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 
-export const authenticate = async (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    const data = jwt.verify(token, TOKEN_KEY)
-    res.locals.user = data
-    // if (compare(req))
-    next()
-  } catch (error) {
-    res.status(403).send({ error: 'Unauthorized' })
+export default class AuthService {
+  authenticate = async (req, res, next) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      const data = jwt.verify(token, TOKEN_KEY)
+      res.locals.user = data
+      // if (compare(req))
+      next()
+    } catch (error) {
+      res.status(403).send({ error: 'Unauthorized' })
+    }
   }
-}
 
-export const verifyToken = async (req, res) => {
-  try {
-    const token = req.headers.authorization.split(' ')[1]
-    const user = jwt.verify(token, TOKEN_KEY)
-    res.json(user)
-  } catch (error) {
-    res.status(403).send({ error: 'Unauthorized' })
+  verifyToken = async (req, res) => {
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      const user = jwt.verify(token, TOKEN_KEY)
+      res.json(user)
+    } catch (error) {
+      res.status(403).send({ error: 'Unauthorized' })
+    }
   }
-}
 
-export const signToken = payload => {
-  const token = jwt.sign(
-    { payload, exp: Math.floor(new Date().getTime() / 1000) + 42 * 3600 },
-    TOKEN_KEY
-  )
-  return token
-}
-
-export const VerifyPassword = async (user, password, res) => {
-  try {
-    return await bcrypt.compare(password, user.password_digest)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  signToken = payload => {
+    const token = jwt.sign(
+      { payload, exp: Math.floor(new Date().getTime() / 1000) + 42 * 3600 },
+      TOKEN_KEY
+    )
+    return token
   }
-}
 
-export const HashPassword = async (password, res) => {
-  try {
-    return await bcrypt.hash(password, SALT_ROUNDS)
-  } catch (error) {
-    res.status(500).json({ error: error.message })
+  VerifyPassword = async (user, password, res) => {
+    try {
+      return await bcrypt.compare(password, user.password_digest)
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
+  }
+
+  HashPassword = async (password, res) => {
+    try {
+      return await bcrypt.hash(password, SALT_ROUNDS)
+    } catch (error) {
+      res.status(500).json({ error: error.message })
+    }
   }
 }
--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,48 +1,99 @@
 import { User } from '../database'
-import { signToken, HashPassword, VerifyPassword } from '../auth'
+import AuthService from '../auth'
 
-export const loginUser = async (req, res) => {
-  try {
-    const user = await User.findOne({ username: req.body.username })
-    if (await VerifyPassword(user, req.body.password)) {
+export default class UserController {
+  constructor() {
+    this.Authenticate = new AuthService()
+  }
+  async loginUser(req, res) {
+    try {
+      const user = await User.findOne({ username: req.body.username })
+      if (await VerifyPassword(user, req.body.password)) {
+        const payload = {
+          _id: user._id,
+          name: user.name,
+          email: user.email
+        }
+        const token = signToken(payload)
+        res.status(200).send({ payload, token })
+      } else {
+        res.status(401).json({ error: 'Token Invalid' })
+      }
+    } catch (error) {
+      res.status(401).json({ error: 'Token Invalid' })
+    }
+  }
+
+  async registerUser(req, res) {
+    try {
+      const { name, email, username, password } = req.body
+      const password_digest = await HashPassword(password, res)
+
+      const user = new User({
+        name,
+        username,
+        email,
+        password_digest
+      })
+
       const payload = {
         _id: user._id,
         name: user.name,
-        email: user.email
+        username: user.username
       }
       const token = signToken(payload)
-      res.status(200).send({ payload, token })
-    } else {
+      if (token) {
+        await user.save()
+        res.json({ payload, token })
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message.split(':')[0] })
+    }
+  }
+
+  async loginUser(req, res) {
+    try {
+      const user = await User.findOne({ username: req.body.username })
+      if (await VerifyPassword(user, req.body.password)) {
+        const payload = {
+          _id: user._id,
+          name: user.name,
+          email: user.email
+        }
+        const token = signToken(payload)
+        res.status(200).send({ payload, token })
+      } else {
+        res.status(401).json({ error: 'Token Invalid' })
+      }
+    } catch (error) {
       res.status(401).json({ error: 'Token Invalid' })
     }
-  } catch (error) {
-    res.status(401).json({ error: 'Token Invalid' })
   }
-}
 
-export const registerUser = async (req, res) => {
-  try {
-    const { name, email, username, password } = req.body
-    const password_digest = await HashPassword(password, res)
+  async registerUser(req, res) {
+    try {
+      const { name, email, username, password } = req.body
+      const password_digest = await HashPassword(password, res)
 
-    const user = new User({
-      name,
-      username,
-      email,
-      password_digest
-    })
+      const user = new User({
+        name,
+        username,
+        email,
+        password_digest
+      })
 
-    const payload = {
-      _id: user._id,
-      name: user.name,
-      username: user.username
+      const payload = {
+        _id: user._id,
+        name: user.name,
+        username: user.username
+      }
+      const token = signToken(payload)
+      if (token) {
+        await user.save()
+        res.json({ payload, token })
+      }
+    } catch (error) {
+      res.status(500).json({ error: error.message.split(':')[0] })
     }
-    const token = signToken(payload)
-    if (token) {
-      await user.save()
-      res.json({ payload, token })
-    }
-  } catch (error) {
-    res.status(500).json({ error: error.message.split(':')[0] })
   }
 }
