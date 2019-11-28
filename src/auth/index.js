201b33bb75eee0374373b21ddcc9951dc809e312
Andre Pato <anpato1994@gmail.com>
2019-11-28 13:33:54 -0500

    fixed bug in controllers

--- src/auth/index.js
+++ src/auth/index.js
@@ -8,7 +8,6 @@ const SALT_ROUNDS = parseInt(process.env.SALT_ROUNDS)
 
 export default class AuthController {
   async Authenticate(req, res, next) {
-    console.log(req)
     try {
       const token = req.headers.authorization.split(' ')[1]
       const data = jwt.verify(token, TOKEN_KEY)
