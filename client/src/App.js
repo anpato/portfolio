8339aa36b3daa78f640b5f3146c44c5cfb6439fd
Andre Pato <anpato1994@gmail.com>
2019-12-01 17:01:27 -0500

    added responsveness to project, added links to project

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Routes from './routes/Routes'
 import NavBar from './components/NavBar'
+import { getToken } from './services/TokenService'
 
 function App() {
   const [darkTheme, setTheme] = useState(false)
   const [authenticated, setAuthentication] = useState(false)
+  useEffect(() => {
+    const token = getToken()
+    if (token) {
+      setAuthentication(true)
+    }
+  }, [])
   return (
     <div
       className={`${darkTheme ? 'theme--dark' : 'theme--light'} theme--wrapper`}
